b7664cd1232725be57d9efc0d607a1f81d48c75b
chenzw <656469722@qq.com>
2019-09-19 18:03:57 +0800

    initialization

---
+++ easy-auth-api/src/main/java/cn/chenzw/auth/easy/api/EasyAuthApiApp.java
@@ -0,0 +1,12 @@
+package cn.chenzw.auth.easy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAuthApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyAuthApiApp.class, args);
+    }
+}
--- easy-auth-api/src/main/java/cn/chenzw/auth/easy/api/controllers/EasyUserAuthenticationEndpoint.java
+++ easy-auth-api/src/main/java/cn/chenzw/auth/easy/api/controllers/EasyUserAuthenticationEndpoint.java
@@ -2,18 +2,20 @@ package cn.chenzw.auth.easy.api.controllers;
 
 import cn.chenzw.auth.easy.api.service.EasyUserAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Configuration
 public class EasyUserAuthenticationEndpoint {
 
 
     @Autowired
     EasyUserAuthenticationService userAuthenticationService;
 
-    @GetMapping("${easy.auth.login-uri}")
+    @GetMapping(value = "${easy.auth.login-uri}")
     public void login() {
         userAuthenticationService.login();
     }

25cc2cda83c7f6f20fc6a320fa0bc9565bb490d6
chenzw <656469722@qq.com>
2019-09-19 11:35:26 +0800

    initialization

---
+++ easy-auth-core/src/main/java/cn/chenzw/auth/easy/core/examples/DefaultEasyUserAuthentication.java
@@ -0,0 +1,23 @@
+package cn.chenzw.auth.easy.core.examples;
+
+import cn.chenzw.auth.easy.core.core.AbstractEasyUserAuthentication;
+import cn.chenzw.auth.easy.core.definition.UserAuthenticationDefinition;
+
+/**
+ * 默认认证
+ *
+ * @author chenzw
+ */
+public class DefaultEasyUserAuthentication extends AbstractEasyUserAuthentication {
+
+
+    @Override
+    public boolean checkUsernameAndPassword(UserAuthenticationDefinition userAuthenticationDefinition) {
+        return true;
+    }
+
+    @Override
+    public boolean checkCaptcha(UserAuthenticationDefinition userAuthenticationDefinition) {
+        return true;
+    }
+}
